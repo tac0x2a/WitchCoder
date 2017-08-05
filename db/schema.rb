b8f222eddcf7a8a1da541e5ae41fbfb9cbf7604c
TAC <tac@tac42.net>
2017-08-05 19:48:58 +0900

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804165800) do
+ActiveRecord::Schema.define(version: 20170805100335) do
+
+  create_table "problems", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.integer "version"
+    t.string "publish_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_problems_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
+  # def setup
+  #   @user = users("test")
+  #   @problem = problems(user_id: @user.id)
+  # end
+  #
   # test "the truth" do
   #   assert true
   # end
